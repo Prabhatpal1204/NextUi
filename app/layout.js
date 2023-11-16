aa9e47ecc8b07f2cb602fdec6b4614e7fa4b1454
Prabhat <prabhat.pal1204@gmail.com>
2023-11-17 02:04:44 +0530

    Chnaged Favicon

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ahead App",
